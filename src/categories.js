a450cef64355fbc521b0aab642ec764f666e5b4a
Elvira Gizatullina <elvirawed@gmail.com>
2025-01-12 18:27:59 +0300

    Adding components and data for homepage

---
+++ src/categories.js
@@ -0,0 +1,40 @@
+const categories = [
+  {
+    id: 1,
+    title: "Womens",
+    imageUrl:
+      "https://images.lululemon.com/is/image/lululemon/na_May24_Q2_W_HoodiesSweatshirts_1_3_Med_MediaAction_D?wid=750&op_usm=0.8,1,10,0&fmt=webp&qlt=80,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72",
+  },
+  {
+    id: 2,
+    title: "Mens",
+    imageUrl:
+      "https://images.lululemon.com/is/image/lululemon/LU9BUMS_066854_2?wid=1080&op_usm=0.5,2,10,0&fmt=webp&qlt=80,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72",
+  },
+  {
+    id: 3,
+    title: "Hoodies",
+    imageUrl:
+      "https://images.lululemon.com/is/image/lululemon/NA_DecWk3_W_HoodiesSweatshirts_EPS_CarouselCard_D_Softstreme2?wid=624&op_usm=0.8,1,10,0&fmt=webp&qlt=80,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72",
+  },
+  {
+    id: 4,
+    title: "Joggers",
+    imageUrl:
+      "https://images.lululemon.com/is/image/lululemon/LM5BBRS_033123_1?wid=1080&op_usm=0.5,2,10,0&fmt=webp&qlt=80,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72",
+  },
+  {
+    id: 5,
+    title: "Shoes",
+    imageUrl:
+      "https://images.lululemon.com/is/image/lululemon/LW9EPZS_062999_6?wid=1080&op_usm=0.5,2,10,0&fmt=webp&qlt=80,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72",
+  },
+  {
+    id: 6,
+    title: "Bags",
+    imageUrl:
+      "https://images.lululemon.com/is/image/lululemon/LU9B15S_064762_2?wid=1080&op_usm=0.5,2,10,0&fmt=webp&qlt=80,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72",
+  },
+];
+
+export default categories;
